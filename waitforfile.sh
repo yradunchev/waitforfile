392e1b21626f487bf7bcf2114436df48e30d39a9
Yordan Radunchev <yordan@radunchev.com>
2014-01-07 16:25:00 +0200

    removed mail signature

--- waitforfile.sh
+++ waitforfile.sh
@@ -22,12 +22,3 @@ do
 #move file to destination directory
         mv ${i} ${DSTFL}
 done
-
-
-The contents of this e-mail and attachments, if any, are confidential and solely intended for the use of the addressee. If you receive this e-mail in error, then we kindly request you to notify the sender thereof immediately, and to delete the e-mail and the attachments without printing, copying or distributing any of those.
-
-The publication, copying whole or in part or use or dissemination in any other way of the e-mail and attachments by others than the intended person(s) is prohibited.
-
-The sender cannot guarantee the security of electronic communication and is not liable for any negative consequence of the use of electronic communication, including but not limited to, damage as a result of in or non-complete delivery or delay in delivery of any e-mail; the text of the e-mail as sent is decisive.
-
-The sender is part of the TMF Group, consisting of a number of companies worldwide. A full list of the names, addresses and details of the regulatory status of companies is available on the web site: http://www.tmf-group.com.
